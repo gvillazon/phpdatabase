bd944e4e2dcfe4657eeb7bbe8aa2dce797441de3
george villazon <gvillazon152@gmail.com>
2019-01-09 10:52:46 -0500

    test

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 echo "Welcome to OpenShift Online Developer Preview";
 echo "<br>To test the database, hit the dbtest.php URL";
-echo "<br>You mean to execute dbtest.php";
+
 
 echo "<br><br>This assumes that you have the correct env variables set";
-echo "<BR><BR>The environment variables required are databaseuser, databasepassword, and databasenmae.";
+echo "<BR><BR>The environment variables required are MYSQL_USER, MYSQL_PASSWORD, and MYSQL_DATABASE.";
 
