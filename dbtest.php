b1d78c64fc9a9f6e1cd6394d65f4384879395077
george villazon <gvillazon152@gmail.com>
2019-01-09 10:52:29 -0500

    test

--- dbtest.php
+++ dbtest.php
@@ -1,16 +1,15 @@
 <?php
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
+$dbhost = getenv("DATABASE_SERVICE_NAME");
+$dbuser = getenv("MYSQL_USER");
+$dbpwd = getenv("MYSQL_PASSWORD");
+$dbname = getenv("MYSQL_DATABASE");
 
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
-    printf("Connected to the database $databasename hello");
+    printf("Connected to the database");
 }
 $connection->close();
 ?>
