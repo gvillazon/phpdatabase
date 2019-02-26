987cfef379361567460c3fea27447f7e0d336c7e
george villazon <gvillazon152@gmail.com>
2019-02-26 10:32:42 -0500

    change

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <html>
 <body>
-<?php
-$dbhost = getenv('DATABASE_SERVICE_NAME');
-$dbuser = getenv('MYSQL_USER');
-$dbpwd = getenv('MYSQL_PASSWORD');
-$dbname = getenv('MYSQL_DATABASE');
-printf("information host %s user %s, password %s db %s\n", $dbhost, $dbuser, $dbpwd, $dbname);
-
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-} else {
-    printf("Connected to the database");
-}
-$connection->close();
+<?php echo '<p>Hello World</p>';
 ?>
 </body>
 </html>
